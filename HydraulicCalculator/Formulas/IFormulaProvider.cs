69ecdbf204bd639fecea5c2275ea434206eb66fe
deusburn <sky_burn@ukr.net>
2017-01-02 22:25:49 +0200

    Added main logic to project

---
+++ HydraulicCalculator/Formulas/IFormulaProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydraulicCalculator.Formulas
+{
+    interface IFormulaProvider
+    {
+        void Execute();
+        string FormulaName { get; }
+    }
+}
--- HydraulicCalculator/Program.cs
+++ HydraulicCalculator/Program.cs
@@ -1,15 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using HydraulicCalculator.Formulas;
 
 namespace HydraulicCalculator
 {
-    class Program
+    public class Program
     {
+        static List<IFormulaProvider> FormulaProvider = new List<IFormulaProvider>()
+        {
+            
+        };
+
+        static void DoAction(int userReaction)
+        {
+            var provider = FormulaProvider[userReaction - 1];
+            provider.Execute();
+        }
+        static void ShowFunction()
+        {
+            //Console.WriteLine(
+            //    "------------------------------- \n" +
+            //    " The program has a functional : \n\n" +
+            //    " 1. Add post ; \n" +
+            //    " 2. Delete post ; \n" +
+            //    " 3. Show all posts ; \n" +
+            //    " 4. Check note ; \n" +
+            //    " 5. Exit ; \n\n" +
+            //    " Please enter the number :");
+
+            string begin = "------------------------------- \n" +
+                           " The program has a functional : \n\n";
+            string end = " Please enter the number :";
+            string centralPart = "";
+
+            for (int i = 0; i < FormulaProvider.Count; i++)
+            {
+                centralPart += string.Format("{0}. {1} ; \n", i + 1, FormulaProvider[i].FormulaName);
+            }
+            centralPart += string.Format("{0}. {1} ; \n", FormulaProvider.Count + 1, "Exit");
+
+            var result = begin + centralPart + end;
+            Console.WriteLine(result);
+        }
+
         static void Main(string[] args)
         {
+            int userReaction = 0;
+
+            do
+            {
+                try
+                {
+                    ShowFunction();
+
+                    userReaction = int.Parse(Console.ReadLine());
+                    Console.WriteLine();
+
+                    DoAction(userReaction);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(" You entered not correct number !!! ");
+                    Console.WriteLine();
+                }
+            } while (userReaction != GetExitCodeNumber());
+        }
+
+        private static int GetExitCodeNumber()
+        {
+            return FormulaProvider.Count + 1;
         }
     }
 }
